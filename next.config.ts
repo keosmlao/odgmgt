b61ee73c1e2c6d9e0ab7778e5b4629242a57a78b
Mac <keoitc2024@gmail.com>
2026-08-12 13:59:16 +0700

    fff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.0.40.9"],
+  allowedDevOrigins: ["10.0.40.9", "odg.odienmall.com"],
 };
 
 export default nextConfig;
